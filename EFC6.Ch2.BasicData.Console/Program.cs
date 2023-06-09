2bbab92fc90cdb3632866d96a54121b52960eee0
TMoechel <t.moechel@softcreation.de>
2023-06-09 08:59:25 +0200

    Add project files.

---
+++ EFC6.Ch2.BasicData.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using EfC6.Ch2.FitnessApp.Data;
+using EFC6.Ch2.BasicData.Domain;
+
+using (FitnessAppContext context = new FitnessAppContext())
+{
+    context.Database.EnsureCreated();
+}
+AddAcitivity();
+GetRunActivities();
+Console.WriteLine("DB was created and run activities displyed");
+
+void AddAcitivity()
+{
+    var activity = new RunActivity() { Name = "Jogging in the morning", Distance = 100 };
+    var context = new FitnessAppContext();
+    context.RunActivities.Add(activity);
+    context.SaveChanges();
+}
+void GetRunActivities()
+{
+    using var context = new FitnessAppContext();
+    var activities = context.RunActivities.ToList();
+    if (activities.Any())
+        foreach (var activity in activities)
+        {
+            Console.WriteLine($"{activity.Name} {activity.Distance}");    
+        }
+}
---
+++ EfC6.Ch2.FitnessApp.Data/FitnessAppContext.cs
@@ -0,0 +1,17 @@
+﻿using EFC6.Ch2.BasicData.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfC6.Ch2.FitnessApp.Data
+{
+    public class FitnessAppContext : DbContext
+    {
+        public DbSet<RunActivity> RunActivities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server = localhost\\SQLEXPRESS; Database=FitnessDb; Trusted_Connection = True;"
+                );
+        }
+    }
+}
